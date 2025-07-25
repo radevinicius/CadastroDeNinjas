fd06ccfab897e3141fd1648a672cd7edce4b2053
unknown <Vincius Rad>
2025-07-25 11:57:31 -0300

    feat: Adicionado rotas ao NinjaController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -10,6 +8,37 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Boas Vindas !";
+    }
+
+    //ADICIONAR NINJA (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja Criado";
+    }
+
+    //MOSTRAR NINJA POR ID (READ)
+
+    @GetMapping("/todos")
+        public String mostrarTodosOsNinjas(){
+            return "Ninja mostrado";
+    }
+
+    //MOSTRAR TODOS OS NINJAS (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Ninjas mostrados por ID";
+    }
+
+    //ATUALIZAR NINJA (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId(){
+        return "Alterar ninja por ID";
+    }
+
+    //DELETAR NINJA (DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarNinjaPorId(){
+        return "Deletar ninja por ID";
     }
 
 }
